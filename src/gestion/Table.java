59a2c1d35956ca4600bf2d7b1198123e82ec8155
Badet Maxime <badet.maxime@gmail.com>
2021-12-07 12:39:07 +0100

    table attrivute

--- src/gestion/Table.java
+++ src/gestion/Table.java
@@ -1,4 +1,13 @@
 package gestion;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Table {
+    private final int placeParTable = 8;
+
+    private static int id;
+    private int numTable;
+    private int placePrise;
+
 }
