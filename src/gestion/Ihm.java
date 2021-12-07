335f400fcddb84f9cbc7ef64dd77bf538fa48849
Guillaume Bernier <guillaume45Bernier@gmail.com>
2021-12-07 15:22:56 +0100

    Classe Ihm avec l

--- src/gestion/Ihm.java
+++ src/gestion/Ihm.java
@@ -1,5 +1,25 @@
 package gestion;
 
+import java.util.Scanner;
+
 public class Ihm {
+
+    public int demandeIdentite() {
+        Scanner identite = new Scanner(System.in);
+        int id = -1;
+        while (identite.hasNext()) {
+            System.out.println("inscrivez 0 si vous êtes membre du personnel, 1 si vous êtes étudiants");
+            id = identite.nextInt();
+            if (id == 0) {
+                return 0;
+            } else if (id == 1) {
+                return 1;
+            }
+        }
+        return id;
+    }
+
+
+
 
 }
